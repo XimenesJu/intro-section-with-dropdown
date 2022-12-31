84a6de6a0fea3aa6c8c55b57d37d8e10c3da532c
XimenesJu <ju.cx@hotmail.com>
2022-12-31 00:14:14 -0300

    adding image via html instead

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,50 @@
-var img = document.createElement("img"); 
+// function showImage() {
+//   let img = document.getElementById('secimg')
+//   img.innerHTML += img
+//   img.src = '../images/image-hero-desktop.png'
+// }
+
+// var img = document.createElement("img"); 
  
-img.src = './assets/images/image-hero-desktop.png' 
-var src = document.getElementById("x"); 
+// img.src = '../images/image-hero-desktop.png';
+// var src = document.getElementById("x"); 
  
-src.appendChild(img); 
+// src.appendChild(img); 
+
+
+
+// var img = new Image(); 
+// var div = document.getElementById('x'); 
+ 
+ 
+// img.onload = function() { 
+ 
+//   div.innerHTML += '<img src="'+img.src+'" />';  
+ 
+// }; 
+ 
+ 
+ 
+
+// var img = document.createElement("img"); 
+ 
+// img.src = '../images/image-hero-desktop.png'; 
+// var src = document.getElementById("secimg"); 
+ 
+// src.appendChild(img); 
+
+
+
+// var img = new Image(); 
+// var div = document.getElementById('secimg'); 
+ 
+ 
+// img.onload = function() { 
+ 
+//   div.innerHTML += '<img src="'+img.src+'" />';  
+ 
+// }; 
+ 
+ 
+// img.src = '../images/image-hero-desktop.png'; 
 
