9f451f9005b55e91be6b526c186f118b6faf86b2
XimenesJu <ju.cx@hotmail.com>
2023-01-05 22:19:05 -0300

    fixing dropdown to alternate when selected + starting to add icons to dropdown list

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,68 +5,78 @@
 // }
 
 // var img = document.createElement("img"); 
- 
+
 // img.src = '../images/image-hero-desktop.png';
 // var src = document.getElementById("x"); 
- 
+
 // src.appendChild(img); 
 
 
 
 // var img = new Image(); 
 // var div = document.getElementById('x'); 
- 
- 
+
+
 // img.onload = function() { 
- 
+
 //   div.innerHTML += '<img src="'+img.src+'" />';  
- 
+
 // }; 
- 
- 
- 
+
+
+
 
 // var img = document.createElement("img"); 
- 
+
 // img.src = '../images/image-hero-desktop.png'; 
 // var src = document.getElementById("secimg"); 
- 
+
 // src.appendChild(img); 
 
 
 
 // var img = new Image(); 
 // var div = document.getElementById('secimg'); 
- 
- 
+
+
 // img.onload = function() { 
- 
+
 //   div.innerHTML += '<img src="'+img.src+'" />';  
- 
+
 // }; 
- 
- 
+
+
 // img.src = '../images/image-hero-desktop.png'; 
 
-let aberto = false
-const botaozinho = document.querySelectorAll('.botaozinho') 
+
+
+
+// Recebendo elementos do html
+const botaozinho = document.querySelectorAll('.botaozinho')
 const nicolas = document.querySelectorAll('.nicolas')
+let aberto = [false, false]
+// Função que serve pra usar o índice 0 e 1
 for (let indice = 0; indice < botaozinho.length; indice++) {
+  // recebendo o evento de clicar do botãozinho 0 e 1 e chamando a função clicar
   botaozinho[indice].addEventListener('click', clicar)
-function clicar() {
-  if (aberto === false) {
-    nicolas[indice].classList.remove('hide')
-    aberto = true
-    nicolas[indice].style.display = 'block'
-  } 
-  else if (aberto === true) {
-    nicolas[indice].classList.add('hide')
-    aberto = false
-    nicolas[indice].style.display = 'none'
+  function clicar() {
+    if (aberto[indice] === false) {
+      fechartodas()
+      nicolas[indice].classList.remove('hide')
+      aberto[indice] = true
+    }
+    else if (aberto[indice] === true) {
+      nicolas[indice].classList.add('hide')
+      aberto[indice] = false
+    }
   }
 }
+function fechartodas() {
+  nicolas[0].classList.add('hide')
+  aberto[0] = false
+  nicolas[1].classList.add('hide')
+  aberto[1] = false
 }
-
 
 
 
