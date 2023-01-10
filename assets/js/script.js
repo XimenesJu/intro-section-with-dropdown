ee99a374d94a3bf8abd0d672817831c12bc91ee3
XimenesJu <ju.cx@hotmail.com>
2023-01-09 22:34:07 -0300

    starting media query + adding function to monitor the screen size

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,7 +49,21 @@
 // img.src = '../images/image-hero-desktop.png'; 
 
 
+function sizeChanged() {
+  if (window.innerWidth >= 768) {
+    menu.style.display = 'block'
+  } else {
+    menu.style.display = 'none'
+  }
+}
 
+// function clickMenu() {
+//   if (menu.style.display == 'block') {
+//     menu.style.display = 'none'
+//   } else {
+//     menu.style.display = 'block'
+//   }
+//}
 
 // Recebendo elementos do html
 const botaozinho = document.querySelectorAll('.botaozinho')
